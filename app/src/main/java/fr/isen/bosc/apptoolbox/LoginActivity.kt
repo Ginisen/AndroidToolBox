6fb5c821d0ada916b0c2deda4f886ec8c40e0d4c
Ginie <virginie.bosc@isen.yncrea.fr>
2020-01-27 08:59:46 +0100

    Initialisation

--- app/src/main/java/fr/isen/bosc/apptoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/bosc/apptoolbox/LoginActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.bosc.apptoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,15 +8,29 @@ import kotlinx.android.
 
 class LoginActivity : AppCompatActivity() {
 
+     companion object {
+         private const vat GOOD_IDENTIFIANT = "admin"
+         private const vat GOOD_PASSWORD = "123"
+     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_)
 
 
         loginAction.setOnClickListener { it:View!
-            val identifiant = loginUser.text.toString()
+            /*
             val message = "tu as cliqué $identifiant"
-            Toast.makeText(context: this, message, Toast.LENGTH_LONG).show()
+            Toast.makeText(context: this, message, Toast.LENGTH_LONG).show()*/
+            doLogin()
+        }
+    }
+
+    private fun doLogin() {
+        val identifiant = loginUser.text.toString()
+        val password = loginUser.text.toString()
+        if(identifiant == GOOD_IDENTIFIANT && password == GOOD_PASSWORD) {
+            startActivity(Intent(packageContext: this@LogActivity))
         }
     }
 }
