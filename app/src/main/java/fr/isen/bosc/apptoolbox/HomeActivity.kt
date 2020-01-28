32aa7779ea91ec6d8233538fd24c91e9cb96a812
Ginie <virginie.bosc@isen.yncrea.fr>
2020-01-28 22:21:55 +0100

    Sauvegarde

--- app/src/main/java/fr/isen/bosc/apptoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/bosc/apptoolbox/HomeActivity.kt
@@ -1,11 +1,11 @@
 package fr.isen.bosc.apptoolbox
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.activity_login.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,8 +13,24 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        imageCycleDeVie.setOnClickListener {
+        buttonCycleDeVie.setOnClickListener {
             startActivity(Intent(this@HomeActivity, CycleDeVieActivity::class.java))
         }
+
+        logOut.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun logOut() {
+        val sharedPreferences : SharedPreferences = getSharedPreferences(LoginActivity.USER_PREFS, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
+
+        val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/fr/isen/bosc/apptoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/bosc/apptoolbox/LoginActivity.kt
@@ -1,21 +1,34 @@
 package fr.isen.bosc.apptoolbox
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.content.Intent
+import android.content.SharedPreferences
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var sharedPreferences: SharedPreferences
+
     companion object {
         private const val GOOD_IDENTIFIANT = "admin"
         private const val GOOD_PASSWORD = "123"
+        const val USER_PREFS = "user_prefs"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        sharedPreferences = getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE)
+        val savedIdentifier :String? = sharedPreferences.getString("is", "")
+        val savedPassword :String? = sharedPreferences.getString("pwd", "")
+
+        if (savedIdentifier == GOOD_IDENTIFIANT && savedPassword == GOOD_PASSWORD) {
+            goToHome(savedIdentifier, true)
+        }
 
         button.setOnClickListener {
 
@@ -29,7 +42,27 @@ class LoginActivity : AppCompatActivity() {
         val identifier = identifier.text.toString()
         val password = password.text.toString()
         if(identifier == GOOD_IDENTIFIANT && password == GOOD_PASSWORD) {
-            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+            //startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+            savedUserCredential(identifier, password)
+            goToHome(identifier, true)
         }
+        else {
+            Toast.makeText(this@LoginActivity, "Identifier or Password not correct", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun savedUserCredential(identifier: String, password: String) {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("id", identifier)
+        editor.putString("pwd", password)
+        editor.apply()
+    }
+
+    private fun goToHome(identifier: String, clearCache: Boolean) {
+        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+        intent.putExtra("strIdentifier", identifier)
+        if (clearCache) intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        if (clearCache) finish()
     }
 }
